ffb963b83eea51363c652c6c59a22c32d07db31a
hqa5im <87118837+hqa5im@users.noreply.github.com>
2024-06-06 15:09:25 +0300

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+namespace WebScraper {
+    class Program {
+        static void Main (string [] args) {
+            
+        }
+    }
+}
